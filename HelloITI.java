e3c8adf81726da4a3b9ddc4e5bd6cc8dc3ad7843
Ahmed Mahdy <ahmedkharoub45@gmail.com>
2023-12-26 14:41:29 +0200

    Added file HelloITI.java and i wrote in it printHelloITI method

---
+++ HelloITI.java
@@ -0,0 +1,12 @@
+class HelloITI{
+	
+	
+	public static void main(String[] args){
+		
+			printHelloITI();
+	}
+	public static void printHelloITI()
+		{
+		    System.out.println("HelloITI");
+		}	
+}
